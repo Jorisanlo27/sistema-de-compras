05d9c155a5e51432eacf89eee26628d462df7c05
Jorisanlo27 <jorgersantana27@gmail.com>
2024-03-04 22:26:59 -0400

    initialized backend with spring

---
+++ backend - spring/compras/src/test/java/com/joy/compras/ComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joy.compras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
