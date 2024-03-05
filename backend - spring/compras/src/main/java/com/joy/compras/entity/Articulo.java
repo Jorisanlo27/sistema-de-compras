139e32c9f08fa15294a6ad3154b9878ebe957adb
Jorisanlo27 <jorgersantana27@gmail.com>
2024-03-05 18:20:42 -0400

    API articulos

---
+++ backend - spring/compras/src/main/java/com/joy/compras/entity/Articulo.java
@@ -0,0 +1,34 @@
+package com.joy.compras.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "articulos")
+public class Articulo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String descripcion;
+
+    @ManyToOne
+    private Marca marca;
+
+    @ManyToOne
+    private UnidadMedida unidadMedida;
+
+    private int existencia;
+
+    private boolean estado;
+
+}
---
+++ backend - spring/compras/src/main/java/com/joy/compras/service/ArticuloService.java
@@ -0,0 +1,37 @@
+package com.joy.compras.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.joy.compras.entity.Articulo;
+import com.joy.compras.repository.ArticuloRepository;
+
+@Service
+public class ArticuloService {
+
+    @Autowired
+    ArticuloRepository repo;
+
+    public List<Articulo> getAll() {
+        return repo.findAll();
+    }
+
+    public Optional<Articulo> getById(Long id) {
+        return repo.findById(id);
+    }
+    
+    public List<Articulo> getByMarca(String marca) {
+        return repo.findByMarca_Nombre(marca);
+    }
+
+    public void saveOrUpdate(Articulo articulo) {
+        repo.save(articulo);
+    }
+
+    public void delete(Long id) {
+        repo.deleteById(id);
+    }
+}
