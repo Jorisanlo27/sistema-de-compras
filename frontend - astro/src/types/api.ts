a1c609df7221071e3e88fd21cc6a1016e115c7aa
Jorisanlo27 <jorgersantana27@gmail.com>
2024-03-09 03:11:43 -0400

    Guarda en el crud de departamentos

--- frontend - astro/src/types/api.ts
+++ frontend - astro/src/types/api.ts
@@ -1,35 +1,34 @@
 export type Departamento = {
-map(arg0: (departamento: { id: string; nombre: string | null; estado: boolean; }) => any): unknown;
-    id:     number;
+map(arg0: (departamento: { id: number; nombre: string; estado: boolean; }) => any): Departamento;
+    id: number | null;
     nombre: string;
     estado: boolean;
 }
 
 export type Marca = {
-[x: string]: any;
-    id:     number;
+    id: number;
     nombre: string;
     estado: boolean;
 }
 
 export type UnidadMedida = {
-    id:          number;
+    id: number;
     descripcion: string;
-    estado:      boolean;
+    estado: boolean;
 }
 
 export type Articulo = {
-    id:           number;
-    descripcion:  string;
-    marca:        Marca;
+    id: number;
+    descripcion: string;
+    marca: Marca;
     unidadMedida: UnidadMedida;
-    existencia:   number;
-    estado:       boolean;
+    existencia: number;
+    estado: boolean;
 }
 
 export type Proveedor = {
-    id:              number;
-    documento:       number;
+    id: number;
+    documento: number;
     nombreComercial: string;
-    estado:          boolean;
+    estado: boolean;
 }
