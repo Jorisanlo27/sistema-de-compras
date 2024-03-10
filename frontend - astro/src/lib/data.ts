8ececdc0f1c95e5337cd220f86738a553427a6a9
Jorisanlo27 <jorgersantana27@gmail.com>
2024-03-10 17:05:45 -0400

    crud departamento terminado

--- frontend - astro/src/lib/data.ts
+++ frontend - astro/src/lib/data.ts
@@ -1,25 +1,4 @@
-// NOTE: This is where you could wire up your own data providers:
-// GraphQL, Databases, REST APIs, CDNs, proxies, S3, Matrix, IPFS, you name it…
-
-import { API_URL, REMOTE_ASSETS_BASE_URL } from '../app/constants.js';
-import type { Endpoint, EndpointsToOperations } from '../types/entities.js';
-
-export async function fetchData<Selected extends Endpoint>(endpoint: Selected) {
-	const apiEndpoint = `${API_URL}${endpoint}`;
-
-	console.info(`Fetching ${apiEndpoint}…`);
-	return fetch(apiEndpoint)
-		.then(
-			(r) =>
-				r.json() as unknown as Promise<
-					ReturnType<EndpointsToOperations[Selected]>
-				>,
-		)
-		.catch((e) => {
-			console.error(e);
-			throw Error('Invalid API data!');
-		});
-}
+import { REMOTE_ASSETS_BASE_URL } from '../app/constants.js';
 
 // NOTE: These helpers are useful for unifying paths, app-wide
 export function url(path = '') {
