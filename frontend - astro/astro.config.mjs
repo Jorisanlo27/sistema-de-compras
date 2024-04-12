cd9a8cbd1b473ad4e761b206bcdcbf099b4bc154
Jorisanlo27 <jorgersantana27@gmail.com>
2024-04-12 13:06:23 -0400

    add vercel adapter

--- frontend - astro/astro.config.mjs
+++ frontend - astro/astro.config.mjs
@@ -2,18 +2,17 @@ import react from "@astrojs/react";
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
-
+import vercel from "@astrojs/vercel/serverless";
 const DEV_PORT = 2121;
 
+// https://astro.build/config
 export default defineConfig({
   site: process.env.CI ? 'https://themesberg.github.io' : `http://localhost:${DEV_PORT}`,
   base: process.env.CI ? '/flowbite-astro-admin-dashboard' : undefined,
   output: 'server',
-
   server: {
     port: DEV_PORT
   },
-  integrations: [
-    sitemap(), tailwind(), react()
-  ]
+  integrations: [sitemap(), tailwind(), react()],
+  adapter: vercel()
 });
